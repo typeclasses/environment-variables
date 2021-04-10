d61c336aa64f08f3e0879b434a701dd1d4062c4a
Chris Martin <ch.martin@gmail.com>
2021-04-10 09:41:35 -0600

    docs on optional functions

--- VarConversions.hs
+++ VarConversions.hs
@@ -9,10 +9,15 @@ import Data.Function ((.), id)
 import Data.Functor (fmap)
 import Data.Maybe (Maybe (..))
 
-optional :: a -> Var a -> Opt a
+optional ::
+    value -- ^ Value to return when the variable is absent in the environment.
+    -> Var value -- ^ A required environment variable.
+    -> Opt value -- ^ An optional environment variable.
 optional d (Var x f) = Opt x d f
 
-optionalMaybe :: Var a -> Opt (Maybe a)
+optionalMaybe ::
+    Var value -- ^ A required environment variable.
+    -> Opt (Maybe value) -- ^ An optional environment variable. Returns a 'Just' value when the variable is present in the environment. Returns a 'Nothing' value when the variable is absent in the environment.
 optionalMaybe (Var x f) = Opt x Nothing (fmap Just . f)
 
 class ToMulti v
