d72ec917dcd833dbcd975940e504ee06167d0675
Chris Martin <ch.martin@gmail.com>
2021-04-10 09:35:53 -0600

    doc on the 'var' function

--- OneRequiredVar.hs
+++ OneRequiredVar.hs
@@ -7,11 +7,11 @@ import Data.Text (Text)
 import Name (Name)
 
 -- | A single required environment variable.
-data Var value =
-    Var
-      Name -- ^ The name of the environment variable to read.
-      (Text -> Maybe value) -- ^ How to parse the text into a value.
+data Var value = Var Name (Text -> Maybe value)
     deriving stock Functor
 
-var :: Name -> (Text -> Maybe value) -> Var value
+var ::
+    Name -- ^ The name of the environment variable to read.
+    -> (Text -> Maybe value) -- ^ How to parse the text into a value.
+    -> Var value
 var = Var
