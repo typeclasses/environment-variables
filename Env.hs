3b34912592259b1e88364031db52bb7a31b08dfd
Chris Martin <ch.martin@gmail.com>
2021-04-11 02:19:06 -0600

    move Product into Env module

--- Env.hs
+++ Env.hs
@@ -21,22 +21,64 @@ module Env
 
 import EnvData (Environment, pattern EnvironmentList, Item (..), getEnvironment)
 import EnvFunctor (Context (..))
-import MultiVar (Product (..))
 import Name (Name, pattern NameText, pattern NameString)
 import Var (Var (..), Opt (..), var)
 import Problems (EnvFailure, pattern EnvFailureList, OneEnvFailure (..), Problem (..), oneProblemFailure)
 
 import Control.Applicative (Applicative (..))
 import Data.Function ((.), ($))
-import Data.Functor (fmap)
-import Data.Functor.Compose (Compose (Compose), getCompose)
+import Data.Functor (Functor (..), fmap)
 import Data.Map (Map)
 import Data.Maybe (Maybe (..), maybe)
+import Data.String (IsString (fromString))
 import Data.Text (Text)
 import Data.Validation (Validation (Success, Failure), bindValidation)
 import System.IO (IO)
 
 import qualified Data.Text as Text
+
+---
+
+
+{- | The product of multiplying any number of individual environment variables. Construct 'Product' values using 'lift', 'Applicative' combinators, and string overloading. -}
+
+data Product a
+  where
+    UseNoVars :: a -> Product a
+    UseOneVar :: Var a -> Product a
+    UseOneOpt :: Opt a -> Product a
+    UseManyVars :: Product (a -> b) -> Product a -> Product b
+
+instance IsString (Product Text)
+  where
+    fromString = UseOneVar . fromString
+
+instance IsString (Product (Maybe Text))
+  where
+    fromString = UseOneOpt . fromString
+
+instance Functor Product
+  where
+    fmap f = \case
+      UseNoVars x -> UseNoVars (f x)
+      UseOneVar x -> UseOneVar (fmap f x)
+      UseOneOpt x -> UseOneOpt (fmap f x)
+      UseManyVars mf ma -> UseManyVars (fmap (f .) mf) ma
+
+instance Applicative Product
+  where
+    pure = UseNoVars
+
+    (<*>) :: forall a b. Product (a -> b) -> Product a -> Product b
+    mf <*> (multi_a :: Product a) =
+      case mf of
+        UseNoVars (f :: a -> b) -> fmap f multi_a
+        UseOneVar vf -> UseManyVars (UseOneVar vf) multi_a
+        UseOneOpt vf -> UseManyVars (UseOneOpt vf) multi_a
+        UseManyVars (multi_cab :: Product (c -> a -> b)) (v :: Product c) -> UseManyVars multi_cb v
+          where
+            multi_cb :: Product (c -> b)
+            multi_cb = pure (\f c a -> f a c) <*> multi_cab <*> multi_a
 
 ---
 
