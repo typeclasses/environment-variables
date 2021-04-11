9032776b634b72cf51d1c3278bc197716ebe1dce
Chris Martin <ch.martin@gmail.com>
2021-04-11 02:04:54 -0600

    move Lift into Env module

--- Env.hs
+++ Env.hs
@@ -21,9 +21,36 @@ module Env
 
 import EnvData (Environment, pattern EnvironmentList, Item (..), getEnvironment)
 import EnvFunctor (Context (..))
-import MultiVar (Product)
+import MultiVar (Product (..))
 import Name (Name, pattern NameText, pattern NameString)
-import Var (Var, Opt, var)
+import Var (Var (..), Opt (..), var)
 import Problems (EnvFailure, pattern EnvFailureList, OneEnvFailure (..), Problem (..))
 import Readable (Readable (..))
-import VarConversions (optional, optionalMaybe, Lift (..))
+import VarConversions (optional, optionalMaybe)
+
+import Data.Function ((.))
+import Data.Maybe (Maybe (..))
+import Data.Text (Text)
+
+-- | A /lift/ is a trivial function that converts from a smaller type to a more complex one. Since 'lift' is polymorphic in both domain and codomain, explicit type annotations are recommended.
+
+class Lift b a where
+    lift :: a -> b
+
+instance Lift (Product a) (Var a) where
+    lift = OneVar
+
+instance Lift (Product a) (Opt a) where
+    lift = OneOpt
+
+instance Lift (Var Text) Name where
+    lift x = Var x Just
+
+instance Lift (Opt (Maybe Text)) Name where
+    lift x = Opt x Nothing (Just . Just)
+
+instance Lift (Product Text) Name where
+    lift = lift . lift @(Var Text)
+
+instance Lift (Product (Maybe Text)) Name where
+    lift = lift . lift @(Opt (Maybe Text))
--- VarConversions.hs
+++ VarConversions.hs
@@ -20,25 +20,3 @@ optionalMaybe ::
     Var value -- ^ A required environment variable.
     -> Opt (Maybe value) -- ^ An optional environment variable. Returns a 'Just' value when the variable is present in the environment. Returns a 'Nothing' value when the variable is absent in the environment.
 optionalMaybe (Var x f) = Opt x Nothing (fmap Just . f)
-
--- | A /lift/ is a trivial function that converts from a smaller type to a more complex one. Since 'lift' is polymorphic in both domain and codomain, explicit type annotations are recommended.
-class Lift b a where
-    lift :: a -> b
-
-instance Lift (Product a) (Var a) where
-    lift = OneVar
-
-instance Lift (Product a) (Opt a) where
-    lift = OneOpt
-
-instance Lift (Var Text) Name where
-    lift x = Var x Just
-
-instance Lift (Opt (Maybe Text)) Name where
-    lift x = Opt x Nothing (Just . Just)
-
-instance Lift (Product Text) Name where
-    lift = lift . lift @(Var Text)
-
-instance Lift (Product (Maybe Text)) Name where
-    lift = lift . lift @(Opt (Maybe Text))
